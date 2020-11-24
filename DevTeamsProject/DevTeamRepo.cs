73d7d29fbc6318d376d43c2403a241e8dfe2d54d
tannerlemon <tanrlemn@gmail.com>
2020-11-23 20:48:53 -0500

    Add project files.

---
+++ DevTeamsProject/DevTeamRepo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevTeamsProject
+{
+    public class DevTeamRepo
+    {
+        private readonly DeveloperRepo _developerRepo = new DeveloperRepo(); // this gives you access to the _developerDirectory so you can access existing Developers and add them to a team
+        private readonly List<DevTeam> _devTeams = new List<DevTeam>();
+
+        //DevTeam Create
+        //DevTeam Read
+        //DevTeam Update
+        //DevTeam Delete
+
+        //DevTeam Helper (Get Team by ID)
+
+    }
+}
---
+++ DevTeamsProject/DeveloperRepo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevTeamsProject
+{
+    public class DeveloperRepo
+    {
+        private readonly List<Developer> _developerDirectory = new List<Developer>();
+
+        //Developer Create
+        //Developer Read
+        //Developer Update
+        //Developer Delete
+
+        //Developer Helper (Get Developer by ID)
+    }
+}
